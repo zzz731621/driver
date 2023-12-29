3a78480197cfdf9962013a7fab8cb5bcf0265de7
zzz731621 <cc0939065128@gmail.com>
2023-12-29 09:20:16 -0800

    如何將驅動編譯到linux kernel中

---
+++ 03_helloworld_to_kernel/helloworld.c
@@ -0,0 +1,33 @@
+// 包含一些常用到的macro和函數
+#include <linux/init.h>
+#include <linux/module.h>
+
+// 編譯驅動之前要先確保linux kernel已經編譯過
+// 並且架構要跟開發版的相同(arm)，不能是x86
+// 在目錄下執行make就能夠編譯出.ko文件
+// 將該文件複製到開發版下，並使用insmod helloworld.ko來插入module
+// 可以使用lsmod來查看我們載入的module
+// 使用rmmod helloworld來刪除module，注意不用包括.ko副檔名
+
+
+
+// 加載module時，會印出hello world
+static int hello_init( void ) {
+    // kernel當中是使用printk而不是printf，因為在kernel中無法使用C語言Library
+    printk( "hello world\n" );
+    return 0;
+} // int
+
+// 移除module時，會印出bye bye
+static void hello_exit( void ) {
+    // kernel當中是使用printk而不是printf，因為在kernel中無法使用C語言Library
+    printk( "bye bye\n" );
+} // int
+
+
+// 驅動module的入口和出口
+module_init( hello_init );
+module_exit( hello_exit );
+
+// 通知kernel這個module有許可證
+MODULE_LICENSE( "GPL" );

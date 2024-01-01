f378d56203d227e9c3f33824a71bd853e4c90ef5
zzz731621 <cc0939065128@gmail.com>
2024-01-01 07:13:01 -0800

    註冊一個字符設備

---
+++ 10_register_char_driver/.tmp_versions/char_device.mod
@@ -0,0 +1,3 @@
+/home/eric/work/driver/10_register_char_driver/char_device.ko
+/home/eric/work/driver/10_register_char_driver/char_device.o
+
---
+++ 10_register_char_driver/char_device.mod.c
@@ -0,0 +1,39 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x9a454969, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xdc71537f, __VMLINUX_SYMBOL_STR(param_ops_int) },
+	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
+	{ 0xefd6cf06, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr0) },
+	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+	{ 0xd8e484f0, __VMLINUX_SYMBOL_STR(register_chrdev_region) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "A71A18811063193DF3BB957");
